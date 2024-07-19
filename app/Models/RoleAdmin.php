503b110ebf803b00decb87c83c300f2e6ab30e7b
Sandeep Thakkar <sandeepthakkar@topsinfosolutions.com>
2024-07-19 16:22:58 +0530

    fix: create perission

---
+++ app/Models/RoleAdmin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Hexters\HexaLite\Models\HexaAdmin;
+use Hexters\HexaLite\Models\HexaRole;
+use Hexters\HexaLite\Models\HexaRoleAdmin;
+
+class RoleAdmin extends HexaRoleAdmin
+{
+
+}
